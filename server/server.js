cd61d8f1a919616d2bfecc7cee82303ae442678a
Giraud Julemis <giraud.julemis@gmail.com>
2023-01-09 00:27:40 -0500

    build: start express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+require("dotenv").config();
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri);
+
+app.use(bodyParser.json());
+app.use(xss());
+app.use(mongoSanitize());
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on ${port}...`);
+});
