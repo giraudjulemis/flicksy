6e0d848380b9a505bd149d2e8f6773f340c47b01
Giraud Julemis <giraud.julemis@gmail.com>
2023-01-10 20:23:43 -0500

    build: create router and header and navigation components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/Navigation/header";
+import Home from "./components/Home";
+
 const App = () => {
-  return <>Home</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
