b85a97726e9e41000084e46b9c84920178a781c7
Giraud Julemis <giraud.julemis@gmail.com>
2023-01-12 03:09:37 -0500

    build: side drawer component

--- client/src/components/Navigation/header.js
+++ client/src/components/Navigation/header.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import SideDrawer from "./sideNavigation";
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
       >
         Flicksy
       </Link>
+      <SideDrawer />
     </nav>
   );
 };
---
+++ client/src/components/Navigation/sideNavigation.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import MailIcon from "@mui/icons-material/Mail";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import {
+  Box,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
+import Home from "@mui/icons-material/Home";
+
+const SideDrawer = () => {
+  const [state, setState] = useState(false);
+  return (
+    <>
+      <DehazeIcon className="drawer_btn" onClick={() => setState(true)} />
+      <Drawer anchor="right" open={state} onClose={() => setState(false)}>
+        <Box sx={{ width: 200 }}>
+          <List>
+            <ListItemButton
+              component={RouterLink}
+              to="/"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+            <ListItemButton
+              component={RouterLink}
+              to="/contact"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItemButton>
+            <ListItemButton
+              component={RouterLink}
+              to="/auth"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <VpnKeyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sign In" />
+            </ListItemButton>
+            <ListItemButton
+              onClick={() => {
+                alert("sign out");
+                setState(false);
+              }}
+            >
+              <ListItemIcon>
+                <VpnKeyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sign Out" />
+            </ListItemButton>
+            <>
+              <Divider />
+              <ListItemButton
+                component={RouterLink}
+                to="/dashboard"
+                onClick={() => {
+                  alert("sign out");
+                  setState(false);
+                }}
+              >
+                <ListItemIcon>
+                  <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+              </ListItemButton>
+            </>
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  );
+};
+
+export default SideDrawer;
